34bef68fdc69240fd8f47188c5bf23edc2337b5d
elvinaliyev6 <elvinaliyevinfo@gmail.com>
2022-05-23 15:57:56 +0400

    changes

--- src/main/java/com/company/controller/EmployeeController.java
+++ src/main/java/com/company/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.company.controller;
 import com.company.model.Employee;
 import com.company.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class EmployeeController {
@@ -20,28 +22,41 @@ public class EmployeeController {
     EmployeeService service;
 
     @GetMapping("/")
-    public String viewHomePage(Model model){
-        model.addAttribute("listEmployees",service.findAll());
+    public String viewHomePage(Model model) {
+        return findPaginated(1, model);
+    }
+
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo, Model model) {
+        int pageSize = 5;
+
+        Page< Employee > page = service.findPaginated(pageNo, pageSize);
+        List< Employee > listEmployees = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("listEmployees", listEmployees);
         return "index";
     }
 
     @GetMapping("/showNewEmployeeForm")
-    public String showNewEmployeeForm(Model model){
-        Employee employee=new Employee();
-        model.addAttribute("employee",employee);
+    public String showNewEmployeeForm(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
         return "new_employee";
     }
 
     @PostMapping("saveEmployee")
-    public String formSuccess(@ModelAttribute("employee") Employee employee, @Valid Employee emp, BindingResult bindingResult){
+    public String formSuccess(@ModelAttribute("employee") Employee employee, @Valid Employee emp, BindingResult bindingResult) {
 
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "update_employee";
-        }else{
+        } else {
 
-            try{
+            try {
                 service.saveEmployee(employee);
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return "redirect:/";
@@ -50,10 +65,19 @@ public class EmployeeController {
     }
 
     @GetMapping("/showFormUpdate/{id}")
-    public String showFormUpdate(@PathVariable(value = "id") long id,Model model){
-        Employee employee=service.getEmployee(id);
-        model.addAttribute("employee",employee);
+    public String showFormUpdate(@PathVariable(value = "id") long id, Model model) {
+        Employee employee = service.getEmployeeById(id);
+        model.addAttribute("employee", employee);
         return "update_employee";
     }
+
+    @GetMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") long id) {
+        service.deleteEmployeeById(id);
+        return "redirect:/";
+    }
+
+
+
 
 }
--- src/main/java/com/company/service/EmployeeService.java
+++ src/main/java/com/company/service/EmployeeService.java
@@ -3,6 +3,9 @@ package com.company.service;
 import com.company.dao.EmployeeRepository;
 import com.company.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,7 +27,7 @@ public class EmployeeService {
         repo.save(employee);
     }
 
-    public Employee getEmployee(long id){
+    public Employee getEmployeeById(long id){
         Optional<Employee> optional= repo.findById(id);
 
         Employee employee=null;
@@ -35,4 +38,14 @@ public class EmployeeService {
         }
         return employee;
     }
+    
+    public void deleteEmployeeById(long id){
+        repo.deleteById(id);
+    }
+
+    public Page<Employee> findPaginated(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+        return repo.findAll(pageable);
+    }
+
 }
